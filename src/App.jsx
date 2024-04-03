578427e25a2bbc10493760e24f47c3b4f6e2f9a5
fredysomy <fredysomy@gmail.com>
2024-04-03 12:31:15 +0530

    Added nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Doctor from './pages/Doctor';
-import Home from './pages/Home';
-import Pharmacy from './pages/Pharmacy';
-import Profile from './pages/Profile';
-import Reception from './pages/Reception';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Doctor from "./pages/Doctor";
+import Home from "./pages/Home";
+import Pharmacy from "./pages/Pharmacy";
+import Profile from "./pages/Profile";
+import Reception from "./pages/Reception";
+import Nav from "./components/Nav";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/doctor">
-          <Doctor />
-        </Route>
-        <Route path="/pharmacy">
-          <Pharmacy />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/reception">
-          <Reception />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <Nav />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/doctor">
+            <Doctor />
+          </Route>
+          <Route path="/pharmacy">
+            <Pharmacy />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/reception">
+            <Reception />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
