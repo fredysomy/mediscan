00ef7ef9aad706e771d654e35dbd58dd80935aaf
Adithya Dipu <adithyadip77@gmail.com>
2024-04-03 21:33:45 +0530

    i commit

--- src/pages/Pharmacy.jsx
+++ src/pages/Pharmacy.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import './styles.css'; // Import your CSS file for styling
+import backgroundImage from './images.jpeg';
 
 const Pharmacy = () => {
-    return (
-        <div>
-            <h1>Pharmacy Page</h1>
-        </div>
-    );
+  return (
+    <>
+    <div className="container"  >
+      <div className="left-column">
+        
+        <h2>Left Column</h2>
+        <p>This is the content of the left column.</p>
+      </div>
+      <div className="right-column"  >
+        
+        <h2 className='curr'><u>CURRENT PRESCRIPTION</u></h2>
+        <br/>
+        <p>medicine name:</p>
+        <p>dosage:</p>
+        <p>usage period:</p>
+        <br/>
+        <br/>
+        <h2 className='curr'><u>PREVIOUS PRESCRIPTION</u></h2>
+        <br/>
+        <p>medicine name:</p>
+        <p>dosage:</p>
+        <p>usage period:</p>
+      </div>
+      
+    </div>
+    
+    
+</>
+   
+  );
 }
 
 export default Pharmacy;
